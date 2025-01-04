c2f66cd8ec37715a0dec9eb1f4173e37295e84c6
p33m5t3r <bcl4rk@protonmail.com>
2025-01-04 13:57:27 -0700

    compiler just needs templating and md parser

--- src/compile.hs
+++ src/compile.hs
@@ -2,26 +2,18 @@ module Compile where
 import System.IO (FilePath, readFile)
 import System.FilePath ((</>))
 import qualified Data.Map as Map
-import Control.Monad (forM_, forM)
+import Control.Monad (forM_, forM, unless)
 import Data.Maybe (fromMaybe)
 import Control.Exception (try)
 import System.Directory (listDirectory
                         ,createDirectoryIfMissing
+                        ,doesDirectoryExist
                         )
 
 
--- TODO: build the compiler
+-- TODO:
 -- refactor to data.Text
-
-{-
-rn it should literally just rebuild everything
-eventually we can just track in a file what
-... does and doesn't need rebuilding
-we can read that file in the main compile fn
-so it's type signature wouldn't even change
-
-
--}
+-- track changes somewhere so we don't have to rebuild everything
 
 {- layout
 app.hs
@@ -48,13 +40,6 @@ compiled/
     some_page.html
 -}
 
-
-{- build process
-1. compile posts, get metadata, urls
-2. compile index.html
-3. deal with static later
--}
-
 type PostMetadata = Map.Map String String
 
 testMeta :: PostMetadata
@@ -76,18 +61,27 @@ singletonContext k v = "context: " ++ v
 
 template :: String -> Context -> String
 template t c = t ++ " templated w/ " ++ c
+-- ==============
 
+indexContextFrom :: [PostMetadata] -> Context
+indexContextFrom _ = "index context"
 
 compileIndex :: AppConfig -> [PostMetadata] -> IO ()
-compileIndex _ _ = return ()
--- ==============
+compileIndex cfg metadata =
+    readFile templatePath   -- WARNING: lazy, unsafe
+    >>= (\t -> writeFile outPath $ template t $ indexContextFrom metadata)
+    >> putStrLn ("compiled " ++ outPath)
+    where outPath      = compiledDir cfg </> "index.html"
+          templatePath = templateDir cfg </> "index.html"
+
 
 compilePost :: FilePath -> FilePath -> FilePath -> IO PostMetadata
 compilePost postPath outPath templatePath = do
-    p <- readFile postPath       -- WARNING: lazy
+    p <- readFile postPath       -- WARNING: lazy, unsafe
     t <- readFile templatePath
     let (innerHtml, meta) = parsePost p
-    -- writeFile outPath $ template t $ singletonContext "post" innerHtml
+    writeFile outPath $ template t $ singletonContext "post" innerHtml
+    putStrLn $ "compiled " ++ postPath ++ " to " ++ outPath ++ " with " ++ templatePath
     return meta
 
 -- TODO: make this actually robust
@@ -100,11 +94,10 @@ compilePosts :: AppConfig -> IO [PostMetadata]
 compilePosts cfg = do
     files <- listDirectory (postDir cfg)
     let postPaths = map makePostPaths files
-    forM_ postPaths print  -- for debugging
-    -- forM postPaths $ \(src, dest, template) -> 
-    --    compilePost src dest template
+    -- forM_ postPaths print  -- for debugging
+    forM postPaths $ \(src, dest, template) -> 
+        compilePost src dest template
 
-    return [Map.empty]
   where
     postTemplate = "post.html"
     makePostPaths file = 
@@ -122,26 +115,21 @@ compile = do
         , templateDir   = "templates"
         , compiledDir   = "compiled"
         }
-    
 
     ensureDirectory $ compiledDir cfg
 
     postData <- compilePosts cfg
     compileIndex cfg postData
 
-    forM_ postData print
-
 
 ensureDirectory :: FilePath -> IO Bool
 ensureDirectory path = do
+    existed <- doesDirectoryExist path
     result <- try (createDirectoryIfMissing True path) :: IO (Either IOError ())
     case result of
         Left err -> do
             putStrLn $ "Error creating directory " ++ path ++ ": " ++ show err
             return False
         Right _ -> do
-            putStrLn $ "Directory " ++ path ++ " created/verified successfully"
+            unless existed $ putStrLn $ "Created directory \"" ++ path ++ "\""
             return True
-
-
-
